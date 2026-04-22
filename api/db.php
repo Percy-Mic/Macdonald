e034c8378c05f7f93a3eb918d8ee0302a159eeaf
Percy-Mic <percymicnono@gmail.com>
2026-04-22 13:58:36 +0800

    Create db.php

---
+++ api/db.php
@@ -0,0 +1,17 @@
+<?php
+$host = "mysql-62ea972-percymicnono-12e0.g.aivencloud.com";
+$port = "18500"; 
+$dbname = "defaultdb";
+$user = "avnadmin";
+$password = "dbPass";
+
+try {
+    $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
+    $pdo = new PDO($dsn, $user, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
