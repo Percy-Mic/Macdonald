adfd3157dcdd5d2ac6a7a0dab73400d8475496d5
Percy-Mic <percymicnono@gmail.com>
2026-04-22 16:59:53 +0800

    Update db.php

--- api/db.php
+++ api/db.php
@@ -3,7 +3,7 @@ $host = "mysql-62ea972-percymicnono-12e0.g.aivencloud.com";
 $port = "18500"; 
 $dbname = "defaultdb";
 $user = "avnadmin";
-$password = getenv('db_Pass');
+$password = getenv('dbPass');
 
 try {
     $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
