ec44b37458e3718336b4d2017df1fdfe3b02939a
Percy-Mic <percymicnono@gmail.com>
2026-04-22 14:14:06 +0800

    Change password to use getenv for security
    Updated database password retrieval to use environment variable.

--- api/db.php
+++ api/db.php
@@ -3,7 +3,7 @@ $host = "mysql-62ea972-percymicnono-12e0.g.aivencloud.com";
 $port = "18500"; 
 $dbname = "defaultdb";
 $user = "avnadmin";
-$password = "dbPass";
+$password = getenv('db_Pass');
 
 try {
     $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
